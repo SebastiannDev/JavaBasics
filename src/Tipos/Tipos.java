0200c8cf5c10b6b07a22c468cbe7a350a8fcdce6
Sebastian R <sebasti1959@hotmail.com>
2023-01-19 23:39:52 -0500

    first commit

---
+++ src/Tipos/Tipos.java
@@ -0,0 +1,33 @@
+package Tipos;
+
+public class Tipos {
+  public static void main(String[] args) {
+
+    // 1. Numericos
+    byte var1 = 5;
+    short var2 = 10;
+    int var3 = 255;
+    long var4 = 1235;
+
+    // 1.2 Decimales
+    float var5 = 2.1f;
+    double var6 = 3.32;
+
+    // 2 Booleanos
+    boolean var7 = true;
+    boolean var8 = false;
+
+    // 3 Texto
+    char var9 = 'a';
+    String var10 = "Hola mundo";
+
+    System.out.println(var4);
+    System.out.println(var5);
+    System.out.println(var6);
+    System.out.println(var7);
+    System.out.println(var8);
+    System.out.println(var9);
+    System.out.println(var10);
+
+  }
+}
