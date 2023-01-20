e65655337e7514ca87cdda143fcebde4658f331f
Sebastian <sebastiajhoan72@outlook.es>
2023-01-19 23:55:02 -0500

    print all data primitives

--- src/Tipos/Tipos.java
+++ src/Tipos/Tipos.java
@@ -21,6 +21,9 @@ public class Tipos {
     char var9 = 'a';
     String var10 = "Hola mundo";
 
+    System.out.println(var1);
+    System.out.println(var2);
+    System.out.println(var3);
     System.out.println(var4);
     System.out.println(var5);
     System.out.println(var6);
